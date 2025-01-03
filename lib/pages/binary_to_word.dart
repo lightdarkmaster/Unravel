49d3dd6b6faa7a25b49b55e058e399ed8bbf28ea
lightdarkmaster <79690406+lightdarkmaster@users.noreply.github.com>
2025-01-03 09:43:54 +0800

    Add icons, add Programmer Calculator, configure Homescreen UI grid app display

--- lib/pages/binary_to_word.dart
+++ lib/pages/binary_to_word.dart
@@ -5,6 +5,7 @@ class BinaryToWordPage extends StatefulWidget {
   const BinaryToWordPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _BinaryToWordPageState createState() => _BinaryToWordPageState();
 }
 
